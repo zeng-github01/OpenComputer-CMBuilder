ac1935867b2e959d6d9afa0ebc1ba7eb038f5ea4
Zengyj <zengyj11111@gmail.com>
2024-04-01 15:33:04 +0800

    update

--- lib/Recipe.lua
+++ lib/Recipe.lua
@@ -16,20 +16,20 @@ local function getCraftingPattern(row)
     local materials = {}
     local catalyst
     local product
-    for i = 1, 7 do -- 前7个格子是原材料
+    for i = 1, 7 do  -- 前7个格子是原材料
         local item = database.get(row * 9 - 9 + i)
         if item then -- 如果格子不为空，则添加到材料列表
             materials[i] = item
         end
     end
     catalyst = database.get(row * 9 - 9 + 8) -- 第8个格子是催化剂
-    product = database.get(row * 9 - 9 + 9) -- 第9个格子是产物
+    product = database.get(row * 9 - 9 + 9)  -- 第9个格子是产物
     return materials, catalyst, product
 end
 
 -- 比对机器人内部物品堆栈与合成表
 local function matchRecipe()
-    for row = 1, 81 / 9 do -- 假设数据库每9个格子为一行
+    for row = 1, 81 / 9 do         -- 假设数据库每9个格子为一行
         local materials, catalyst, product = getCraftingPattern(row)
         local materialMatches = {} -- 用于跟踪每种材料是否匹配
 
@@ -62,7 +62,7 @@ local function matchRecipe()
         -- 如果所有材料都匹配，则返回产物的ID和damage值
         if allMatched then
             local itemName = product.name:match(":(.+)$")
-            return itemName .. "#" .. (product.damage or "") , catalyst -- 返回匹配的合成表产物的ID和damage值
+            return itemName .. "#" .. (product.damage or ""), catalyst  -- 返回匹配的合成表产物的ID和damage值
         end
     end
     return nil -- 没有匹配的合成表
@@ -146,16 +146,21 @@ local function process_cell_content(relativeX, relativeY, relativeZ, cell_conten
 end
 
 local function processRecipe()
+    print("process")
     local recipe = readJson(recipeName)
+    print(recipe)
     for y, zLayer in ipairs(recipe) do
+        print("y" .. y)
         -- 遍历Z层
         for z, xLayer in ipairs(zLayer) do
-          -- 遍历X层
-          for x, block in ipairs(xLayer) do
-            process_cell_content(x,y,z,block)
-          end
+            print("z" .. z)
+            -- 遍历X层
+            for x, block in ipairs(xLayer) do
+                print("process_cell_x" .. x)
+                process_cell_content(x, y, z, block)
+            end
         end
-      end
+    end
 
     if catalyst then
         -- 向上移动四格
